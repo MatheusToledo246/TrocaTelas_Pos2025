2b1d7a53de99b8cba7f0c212af36eb6bab8ec682
Matheus Toledo <matheus.silva256@fatec.sp.gpv.br>
2025-03-23 20:59:59 -0300

    TrocaTela2

---
+++ app/src/main/java/com/example/trocatelas_pos2025/LancamentoActivity2.kt
@@ -0,0 +1,15 @@
+package com.example.trocatelas_pos2025
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LancamentoActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lancamento2)
+
+    }
+}
