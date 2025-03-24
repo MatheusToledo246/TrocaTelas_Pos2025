0b4022a641146e29a43c0e30e0e20cab5fd35594
Matheus Toledo <matheus.silva256@fatec.sp.gpv.br>
2025-03-23 21:02:41 -0300

    TrocaTela2

--- app/src/main/java/com/example/trocatelas_pos2025/LancamentoActivity2.kt
+++ app/src/main/java/com/example/trocatelas_pos2025/LancamentoActivity2.kt
@@ -10,6 +10,5 @@ class LancamentoActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lancamento2)
-
     }
 }
